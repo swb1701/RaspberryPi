dd5c81e301fa4b5fe4ffc085f3aa99fad108ef38
swb1701 <scott_bennett@sra.com>
2014-11-23 11:45:58 -0500

    sound meter script

---
+++ soundmeter/soundmeter.py
@@ -0,0 +1,26 @@
+import sys
+import usb.core
+import requests
+import time
+
+streams="Scott's Sound Level Meter:i"
+tokens="<tokencode>"
+
+dev=usb.core.find(idVendor=0x16c0,idProduct=0x5dc)
+
+assert dev is not None
+
+print dev
+
+print hex(dev.idVendor)+','+hex(dev.idProduct)
+
+while True:
+    time.sleep(1)
+    ret = dev.ctrl_transfer(0xC0,4,0,0,200)
+    dB = (ret[0]+((ret[1]&3)*256))*0.1+30
+    print dB
+    msg="{'dB':'"+str(dB)+"'}"
+    try:
+requests.post('https://temporacloud.com/connection/clientSend', data={'streams':streams,'tokens':tokens,'mess
+age':msg},verify=False)
+    except: None 
